51d6fbad4151102b8c63a0670c57dc961ede33b2
Lukas <lukas.ikvm1@gmail.com>
2022-01-15 16:47:07 +0100

    Add score() should return total score test

--- src/main/java/com/example/bowling/Game.java
+++ src/main/java/com/example/bowling/Game.java
@@ -1,5 +1,12 @@
 package com.example.bowling;
 
+import java.util.Random;
+
 public class Game {
 
+    int totalScore = 20;
+
+    public int score() {
+        return totalScore;
+    }
 }
--- src/test/java/com/example/bowling/GameTest.java
+++ src/test/java/com/example/bowling/GameTest.java
@@ -7,6 +7,15 @@ import static org.junit.jupiter.api.Assertions.*;
 class GameTest {
 
 
+    @Test
+    void ScoreMethodShouldReturnTotalScore() {
+        Game game = new Game();
+
+        int result = game.score();
+        assertEquals(game.totalScore,result);
+    }
+
+
 
     
 }
