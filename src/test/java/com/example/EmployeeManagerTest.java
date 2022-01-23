b0d2742b21b9b49f8d9699467ab9d64b668967ec
Lukas <lukas.ikvm1@gmail.com>
2022-01-23 14:44:13 +0100

    Add test for payEmployees() when a successful payment is completed

--- src/test/java/com/example/EmployeeManagerTest.java
+++ src/test/java/com/example/EmployeeManagerTest.java
@@ -1,5 +1,31 @@
 package com.example;
 
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
+
 class EmployeeManagerTest {
+
+
+    @Test
+    void payEmployeesReturnTrueWhenASuccessfulPaymentIsCompleted() {
+        EmployeeRepository employeeRepository = mock(EmployeeRepository.class);
+        BankService bankService = mock(BankService.class);
+
+        List<Employee> employees = List.of(new Employee("1",20));
+        when(employeeRepository.findAll()).thenReturn(employees);
+        EmployeeManager employeeManager = new EmployeeManager(employeeRepository,bankService);
+        employeeManager.payEmployees();
+
+        assertTrue(employees.get(0).isPaid());
+    }
 
 }
